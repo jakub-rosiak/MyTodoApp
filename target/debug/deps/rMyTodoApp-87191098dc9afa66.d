a2ce53489b9bb614632d3a2267b95ba84d6c9861
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2025-10-20 10:06:56 +0200

    chore: init

---
+++ target/debug/deps/rMyTodoApp-87191098dc9afa66.d
@@ -0,0 +1,5 @@
+/home/cube/Downloads/rMyTodoApp/target/debug/deps/rMyTodoApp-87191098dc9afa66.d: src/main.rs
+
+/home/cube/Downloads/rMyTodoApp/target/debug/deps/librMyTodoApp-87191098dc9afa66.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rMyTodoApp-fdff28e53b48cc79.d
@@ -0,0 +1,5 @@
+/home/cube/Downloads/rMyTodoApp/target/debug/deps/rMyTodoApp-fdff28e53b48cc79.d: src/main.rs
+
+/home/cube/Downloads/rMyTodoApp/target/debug/deps/librMyTodoApp-fdff28e53b48cc79.rmeta: src/main.rs
+
+src/main.rs:
